ef632d2210dfb1e1080fc3254043ff6bcbe45916
craiydev <manuxdoe@gmail.com>
2022-07-09 04:06:53 +0200

    Added regex matching and path placeholders

--- sortify.py
+++ sortify.py
@@ -1,12 +1,12 @@
-from distutils import extension
 import os
 import shutil
 import json
 import datetime
+import re
 
 execpath = os.path.join(os.path.dirname(__file__))
 
-def load_config(config):
+def load_config(config: str) -> list:
     global execpath
     with open(os.path.join(execpath, "configs", f"{config}.json")) as file:
         return json.load(file)
@@ -21,19 +21,27 @@ def create_new_path(old_path: str, dest_path: str) -> str:
         basename = f"{name} ({i}){ext}"
     return os.path.join(dest_path, basename)
 
+def fill_path_variables(path: str) -> str:
+    if "{user}" in path:
+        path = path.format(user = os.path.expanduser("~"))
+    return path
+
 def match_items(items: list, match_by: dict) -> list:
     matches = []
     for item in items:
+        conditions = len(match_by)
+
         if "ext" in match_by and os.path.isfile(item):
-            if any(item.endswith("." + ext) for ext in match_by["ext"]): matches.append(item)
-            continue
+            if any(item.endswith("." + ext) for ext in match_by["ext"]): conditions -= 1
         if "contains" in match_by and os.path.isdir(item):
-            if len(match_items([os.path.join(item, child) for child in os.listdir(item)], match_by["contains"])) > 0: matches.append(item)
-            continue
+            if len(match_items([os.path.join(item, child) for child in os.listdir(item)], match_by["contains"])) > 0: conditions -= 1
         if "type" in match_by:
-            if match_by["type"] == "file" and os.path.isfile(item): matches.append(item)
-            if match_by["type"] == "dir" and os.path.isdir(item): matches.append(item)
-            continue
+            if match_by["type"] == "file" and os.path.isfile(item): conditions -= 1
+            elif match_by["type"] == "dir" and os.path.isdir(item): conditions -= 1
+        if "regex" in match_by:
+            if re.match(match_by["regex"], os.path.basename(item)): conditions -= 1
+        #Only match item if all conditions are met
+        if conditions == 0: matches.append(item)
     return matches
 
 def sort(config = "default", log = 1):
@@ -48,7 +56,7 @@ def sort(config = "default", log = 1):
         items = [item for item in items if item not in matches]
 
         for match in matches:
-            dest = create_new_path(match, rule["dest"])
+            dest = create_new_path(match, fill_path_variables(rule["dest"]))
             if not os.path.exists(os.path.dirname(dest)):
                 os.mkdir(os.path.dirname(dest))
 
